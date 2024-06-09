edf13f8122951f2262b52c2bb7b3a108e61850da
Cleon Kalea <totivonest@gmail.com>
2024-06-10 02:36:10 +0800

    Added Carousel

--- website/frontend/src/components/Sidebar.jsx
+++ website/frontend/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import './Sidebar.css';
 import sidebarLogo from '../assets/landit.png'; // Add your sidebar logo image in the assets folder
-import sidebarLogo2 from '../assets/react.svg'; // Add your sidebar logo image in the assets folder
+
 
 const Sidebar = () => {
   return (
@@ -14,14 +14,14 @@ const Sidebar = () => {
       
       <div className='sidebar-box-1-active'>
         <div className='sidebar-box-2'>
-          <img src={sidebarLogo2} className='sidebar-logo-2'/>
+        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/home.png" alt="Dashboard" className='sidebar-logo-2'/>
         </div>
         <span className="sidebar-title-2">Land Overview</span>
       </div>
       
       <div className='sidebar-box-1-inactive'>
         <div className='sidebar-box-2'>
-          <img src={sidebarLogo2} className='sidebar-logo-2'/>
+          <img src="https://img.icons8.com/windows/96/FFFFFF/total-sales--v1.png" className='sidebar-logo-2'/>
         </div>
         <span className="sidebar-title-2">Price Prediction</span>
       </div>
