1ce6dc84035ac6e1baf950709b92872958736d7b
Wahyu Pramana <wahpram2607@gmail.com>
2024-06-14 12:31:23 +0800

    add dev_config

--- website/backend/config/dev_config.py
+++ website/backend/config/dev_config.py
@@ -1,2 +1,2 @@
-MONGO_URI = 'mongodb://localhost:27017/'
+MONGO_URI = 'mongodb+srv://wahpram2607:Bangli123.@cluster0.yiobiyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 MONGO_DBNAME = 'db_tanah_bali'
