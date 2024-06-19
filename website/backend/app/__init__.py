da35aa9ea52eb36f0b384f2af3925b897d51e5de
Wahyu Pramana <wahpram2607@gmail.com>
2024-06-19 10:34:39 +0800

    new gitignore

--- website/backend/app/__init__.py
+++ website/backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+from app import routes
 
 app = Flask(__name__)
 
@@ -8,5 +9,3 @@ app.config.from_object('config.dev_config')
 
 client = MongoClient(app.config['MONGO_URI'])
 db = client[app.config['MONGO_DBNAME']]
-
-from app import routes
